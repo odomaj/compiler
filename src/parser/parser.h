7f0d2a7587257c684fda7263cdacd498ea0f46a9
odomaj <odomaj@clarkson.edu>
2025-02-04 17:18:35 -0500

    save checkpoint

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -3,6 +3,30 @@
 
 #include "utils.h"
 
-int parse_tree(token_list_t *tree);
+/* GRAMMAR
+ * S  -> P S1
+ * S1 -> '+' P S1 | '-' P S1 | empty
+ * P  -> E P1
+ * P1 -> '*' E P1 | '/' E P1 | empty
+ * E  -> B E1
+ * E1 -> '^' E | empty
+ * B  -> '(' S ')' | int
+ */
+
+typedef struct
+{
+    int8_t return_code;
+    float result;
+} parser_data_t;
+
+parser_data_t parse_tree(token_list_t *tree);
+
+parser_data_t sum(token_list_t *tree);
+parser_data_t sum_help(token_list_t *tree);
+parser_data_t product(token_list_t *tree);
+parser_data_t product_help(token_list_t *tree);
+parser_data_t power(token_list_t *tree);
+parser_data_t power_help(token_list_t *tree);
+parser_data_t bracket(token_list_t *tree);
 
 #endif
